b928efe5b3a5ac1e51282c0734dd57c3d9236f93
MeejinAn <rhacllr@naver.com>
2020-11-08 16:43:01 +0900

    프로그래밍 언어 과제 1주차

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta, date
+
+name = input("과제 이름을 입력해주세요. -> ")
+date1 = input("과제 기한을 입력해주세요. (ex. yyyy.mm.dd) -> ")
+y, m, d = date1.split(".")
+y = int(y)
+m = int(m)
+d = int(d)
+date2 = date(y, m, d)
+tday = date.today()
+
+if (date2 - tday).days == 0:
+    print (name, "D-DAY")
+elif (date2 - tday).days < 0:
+    print ("이미 지났습니다.")
+else:
+    print (name, "D-",(date2 - tday).days)
+
+
+
