4a1d384c748f5facc4f426e3f77ec64b1d26c2b6
MeejinAn <rhacllr@naver.com>
2020-11-08 21:48:23 +0900

    프로그래밍 언어 과제 1주차 수정

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from datetime import datetime, timedelta, date
 
-name = input("과제 이름을 입력해주세요. -> ")
-date1 = input("과제 기한을 입력해주세요. (ex. yyyy.mm.dd) -> ")
-y, m, d = date1.split(".")
-y = int(y)
-m = int(m)
-d = int(d)
-date2 = date(y, m, d)
-tday = date.today()
+while True:
+    name = input("과제 이름을 입력해주세요. -> ")
+    date1 = input("과제 기한을 입력해주세요. (ex. yyyy.mm.dd) -> ")
+    y, m, d = date1.split(".")
+    y = int(y)
+    m = int(m)
+    d = int(d)
+    date2 = date(y, m, d)
+    tday = date.today()
 
-if (date2 - tday).days == 0:
-    print (name, "D-DAY")
-elif (date2 - tday).days < 0:
-    print ("이미 지났습니다.")
-else:
-    print (name, "D-",(date2 - tday).days)
+    if (date2 - tday).days < 0:
+        print ("이미 지났습니다.")
+        continue
+    elif (date2 - tday).days == 0:
+        print (name, "D-DAY")
+        break
+    else:
+        print (name, "D-",(date2 - tday).days)
+        break
 
 
 
