0531591ece646916441281dae2946dcc1c787cf7
MeejinAn <rhacllr@naver.com>
2020-11-29 19:58:08 +0900

    프로그래밍언어 과제 4주차(3주차 오류확인 및 수정)

--- main.py
+++ main.py
@@ -23,25 +23,25 @@ while True:
 '''
 
 while True:
-    date11 = input("기준 일자를 입력해주세요. ex) yyyy.mm.dd -> ")
-    name11 = input("과제 이름을 입력해주세요. -> ")
-    date22 = input("과제 기한을 입력해주세요. ex) yyyy.mm.dd -> ")
-    y1, m1, d1 = date11.split(".")
+    date_1 = input("기준 일자를 입력해주세요. ex) yyyy.mm.dd -> ")
+    name_1 = input("과제 이름을 입력해주세요. -> ")
+    deadline_1 = input("과제 기한을 입력해주세요. ex) yyyy.mm.dd -> ")
+    y1, m1, d1 = date_1.split(".")
     y1 = int(y1)
     m1 = int(m1)
     d1 = int(d1)
-    y2, m2, d2 = date22.split(".")
+    y2, m2, d2 = deadline_1.split(".")
     y2 = int(y2)
     m2 = int(m2)
     d2 = int(d2)
-    date3 = date(y1, m1, d1)
-    date4 = date(y2, m2, d2)
-    if (date4 - date3).days < 0:
+    date_2 = date(y1, m1, d1)
+    deadline_2 = date(y2, m2, d2)
+    if (deadline_2 - date_2).days < 0:
         print ("이미 지났습니다. 다시 입력해주세요")
         continue
-    elif (date4 - date3).days == 0:
-        print (name11, "D-DAY")
+    elif (deadline_2 - date_2).days == 0:
+        print (name_1, "D-DAY")
         break
     else:
-        print (name11, "D-", (date4 - date3).days)
+        print (name_1, "D-", (deadline_2 - date_2).days)
         break
